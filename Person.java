aa51a3f2018ea6df96e76935fa47078ccca0226c
Yaroslav <gondarevyaroslav@gmail.com>
2022-02-13 16:53:04 +0300

    Update class

--- Person.java
+++ Person.java
@@ -1,6 +1,10 @@
 public class Person {
     private String name;
+    private int age;
     public void sayHi(){
         System.out.println("Hi, I'm " + name);
+    }
+    public int getAge(){
+        return age;
     }
 }
